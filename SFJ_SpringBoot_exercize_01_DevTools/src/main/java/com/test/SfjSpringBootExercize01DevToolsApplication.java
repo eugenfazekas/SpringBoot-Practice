40b0da110e3e84cadec10beeaeb2e8aa376367d3
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-05-07 19:11:18 +0300

    Practice: RestController & Devtools

---
+++ SFJ_SpringBoot_exercize_01_DevTools/src/main/java/com/test/SfjSpringBootExercize01DevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfjSpringBootExercize01DevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfjSpringBootExercize01DevToolsApplication.class, args);
+	}
+
+}
