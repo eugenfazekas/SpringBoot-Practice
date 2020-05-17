097a8b41485e09a79a21aaaf68d5ed2b897169ec
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-05-17 22:27:59 +0300

    Practice: JUnit HomeController test

---
+++ SFJ_SpringBoot_exercise_14_JUnit_Init_Basics/src/main/java/com/junit/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.junit.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.junit.service.HomeService;
+
+@RestController
+public class HomeController {
+
+	private HomeService homeService;
+
+	@Autowired
+	public void setHomeService(HomeService homeService) {
+		this.homeService = homeService;
+	}
+	
+	@PostMapping("/")
+	public String index(@RequestBody String name) {
+		return homeService.nameCheck(name);
+	}
+}
---
+++ SFJ_SpringBoot_exercise_14_JUnit_Init_Basics/src/main/java/com/junit/service/HomeService.java
@@ -0,0 +1,12 @@
+package com.junit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+	public String nameCheck(String name) {
+		
+		return "ok";
+	}
+}
