e3c2b9dd3c4242984156185482a0ab912d5cbcf7
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-06-02 16:49:12 +0300

    Practice15: JWT_Authentication init

---
+++ TRC_SpringBoot_exercise_15_JWT_Authentication/src/main/java/com/security/TrcSpringBootExercise15JwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrcSpringBootExercise15JwtAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrcSpringBootExercise15JwtAuthenticationApplication.class, args);
+	}
+
+}
---
+++ TRC_SpringBoot_exercise_15_JWT_Authentication/src/test/java/com/security/TrcSpringBootExercise15JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrcSpringBootExercise15JwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
