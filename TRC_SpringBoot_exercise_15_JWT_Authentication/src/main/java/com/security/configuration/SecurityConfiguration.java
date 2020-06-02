c8f9d168f46d46825c19052a06387fb183c1d8ae
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-06-02 20:10:45 +0300

    Practice15: added JWTAuthorizationFilter and Security Configurations

--- TRC_SpringBoot_exercise_15_JWT_Authentication/src/main/java/com/security/configuration/SecurityConfiguration.java
+++ TRC_SpringBoot_exercise_15_JWT_Authentication/src/main/java/com/security/configuration/SecurityConfiguration.java
@@ -7,10 +7,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.security.Repository.UserRepository;
 import com.security.service.UserPrincipalDetailsService;
 
 @Configuration
@@ -18,12 +20,15 @@ import com.security.service.UserPrincipalDetailsService;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
    	
 	 private UserPrincipalDetailsService userPrincipalDetailsService;
+	 private UserRepository userRepository;
 
-	    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService) {
-	        this.userPrincipalDetailsService = userPrincipalDetailsService;
-	    }
+	    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService,
+			UserRepository userRepository) {
+		this.userPrincipalDetailsService = userPrincipalDetailsService;
+		this.userRepository = userRepository;
+	}
 
-	    @Override
+		@Override
 	    protected void configure(AuthenticationManagerBuilder auth) {
 	        auth.authenticationProvider(authenticationProvider());
 	    }
@@ -31,8 +36,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	 @Override
      protected void configure(HttpSecurity http) throws Exception {
 	        http
+	        		.csrf().disable()
+	        		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	        		.and()
+	        		.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+	        		.addFilter(new JWTAuthorizationFilter(authenticationManager(), userRepository))
 	                .authorizeRequests()
-	                .antMatchers("*").permitAll();
+	                .antMatchers("/login").permitAll()
+	                .antMatchers("/api/public/admin/**").hasAnyRole("ADMIN")
+	        		.antMatchers("/api/public/management/**").hasAnyRole("MANAGER");
 
 	    }
 	  	   
