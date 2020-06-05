4f70d70832371f8376b1194965b4eaf5093146f2
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-06-05 19:12:08 +0300

    Practice:17: MongoDB

---
+++ TRC_SpringBoot_exercise_17_MongoDB_1/src/main/java/com/mongodb/HotelRepository.java
@@ -0,0 +1,20 @@
+package com.mongodb;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface HotelRepository extends MongoRepository <Hotel,Long>, QuerydslPredicateExecutor<Hotel> {
+			   
+    List<Hotel> findByPricePerNightLessThan(int maxPrice);
+
+    @Query(value = "{address.city:?0}")
+    List<Hotel> findByCity(String city);
+    
+    Hotel findById(String id);
+}
---
+++ TRC_SpringBoot_exercise_17_MongoDB_1/src/main/java/com/mongodb/TrcSpringBootExercise17MongoDb1Application.java
@@ -0,0 +1,13 @@
+package com.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrcSpringBootExercise17MongoDb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrcSpringBootExercise17MongoDb1Application.class, args);
+	}
+
+}
